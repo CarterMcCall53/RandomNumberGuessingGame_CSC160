d9fffcfad93c1532a0c477cad7d673fd3ffdfe9a
Carter McCall <Carter_McCall53@outlook.com>
2022-09-14 09:43:37 -0600

    Add project files.

---
+++ RandomNumberGame/RandomNumberGame.cpp
@@ -0,0 +1,67 @@
+/* RandomNumberGame
+*  Carter McCall
+*  14 September 2022
+*  Purpose:
+*		Create a random number guessing game
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	string input;
+	char repChar;
+	int correctNumber, playerGuess, diff, absDiff;
+	srand(time(0));
+	do
+	{
+		correctNumber = rand() % 100;
+		for (int i = 0; i < 1;)
+		{
+			cout << "Please Guess: ";
+			cin >> playerGuess;
+			diff = playerGuess - correctNumber;
+			absDiff = abs(diff);
+			if (absDiff > 50)
+			{
+				cout << "Frozen" << endl;
+				continue;
+			}
+			else if (absDiff > 25)
+			{
+				cout << "Cold" << endl;
+				continue;
+			}
+			else if (absDiff > 15)
+			{
+				cout << "Lukewarm" << endl;
+				continue;
+			}
+			else if (absDiff > 10)
+			{
+				cout << "Warm" << endl;
+				continue;
+			}
+			else if (absDiff > 5)
+			{
+				cout << "Hot" << endl;
+				continue;
+			}
+			else if (absDiff > 0)
+			{
+				cout << "Boiling" << endl;
+				continue;
+			}
+			else
+			{
+				cout << "CORRECT! Congratulations!" << endl;
+				i++;
+			}
+		}
+		do {
+			cout << "Do you want to play again? (Y|N): ";
+			cin >> input;
+			repChar = tolower(input[0]);
+		} while (repChar != 'y' && repChar != 'n');
+	} while (repChar == 'y');
+}
